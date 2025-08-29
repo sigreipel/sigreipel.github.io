b0a1a5bc05b476b5ec0054db270697fb485ccf26
Svetlana Greipel <svetlana.greipel@drake.edu>
2025-08-29 16:45:58 -0400

    basic setup

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// Simple smooth scroll for nav links
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
